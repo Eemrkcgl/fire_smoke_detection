f813bb47e285be9fbfc8aaab792b8ac6500d8891
Eemrkcgl <eemrkcgl@gmail.com>
2023-09-18 15:10:22 +0300

    Data paths are adjusted as it should be.

--- train.py
+++ train.py
@@ -4,13 +4,13 @@ from clearml import Task
 task = Task.init(project_name="fire_and_smoke_detection", task_name="detection_v1")
 
 # Load the model.
-model = YOLO('yolov8s.pt')
+model = YOLO('weights/yolov8x.pt')
  
 # Training.
 results = model.train(
-   data='/home/emir/fire_detection/latest_data/data.yaml',
+   data='Latest Data/data.yaml',
    imgsz=640,
-   epochs=15,
+   epochs=20,
    batch=8,
    name='yolov8s_custom'
    )
